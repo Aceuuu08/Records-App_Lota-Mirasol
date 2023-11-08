64a12f19129ef7962f3bcd24be6c7012a4b28249
Aceuuu08 <141605070+Aceuuu08@users.noreply.github.com>
2023-11-08 23:26:09 +0800

    Update config.php

--- config/config.php
+++ config/config.php
@@ -2,7 +2,7 @@
     define('ROOT_URL', 'http:/localhost/recordsapp');
     define('DB_HOST', 'localhost');
     define('DB_USER', 'root');
-    define('DB_PASS', 'root');
+    define('DB_PASS', 'admin12345');
     define('DB_NAME', 'recordsapp')
 
 ?>
